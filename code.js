528405d57055ed675540a9e92fba2d799816818e
Kane Kriz <kanemkriz@gmail.com>
2025-05-10 14:26:43 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,193 @@
+// Kane Kriz
+// UWYO COSC 3020 Algorithms - parallel mergesort primary js code
+// 10 May 2025
 //
 //
-//
-//
-//
-//
+
+
+
+
+
+////
+
+
+
+
+
+function parallelMergeSort(inputArray) 
+{
+    var corePlaceholderCount;
+    var partitionSize;
+    var partitions;
+    var sortedPartitions;
+    var mergedPartitions;
+
+  //
+  
+    var inputLength = inputArray.length;
+    corePlaceholderCount = 2; //alter, test stability. Could somehow be incorporated into test code maybe
+  
+    if(inputLength <= 1) 
+    {
+        return inputArray;
+    }
+
+    partitionSize = Math.ceil(inputLength / corePlaceholderCount); //determined based upon above placeholder account, rather than somehow trying to judge a machines actual core count
+
+  //map
+    partitions = [];
+  
+    for(var i = 0; i < inputLength; i += partitionSize)
+    {
+        var endPos = i + partitionSize;
+      
+        if(endPos > inputLength) 
+        {
+          endPos = inputLength;
+        }
+      
+        partitions.push(inputArray.slice(i, endPos));
+    }
+
+    var partitionsLength = partitions.length;
+  
+    sortedPartitions = partitions.map(function(partition) 
+    {
+        return solvePartition(partition); 
+    });
+
+  
+  //reduce  
+    var sortedPartsLength = sortedPartitions.length;
+  
+    while(sortedPartsLength > 1)
+    {
+        mergedPartitions = [];
+      
+        for(var i = 0; i < sortedPartsLength; i += 2)
+        {
+            if(i + 1 < sortedPartsLength)
+            {
+                mergedPartitions.push(reduceMerge(sortedPartitions[i], sortedPartitions[i + 1]));
+            }
+              
+            else
+            {
+                mergedPartitions.push(sortedPartitions[i]);
+            }
+        }
+      
+        sortedPartitions = mergedPartitions;
+        sortedPartsLength = sortedPartitions.length;
+    }
+
+    return sortedPartitions[0];
+}
+
+
+////
+
+
+
+function solvePartition(partition) 
+{
+    var partitionSize = partition.length;
+    var currentSize;
+    var startPos;
+    var midpointPos;
+    var endPos;
+    var leftPos;
+    var rightPos;
+    var temp;
+
+  //
+
+    for(currentSize = 1; currentSize < partitionSize; currentSize *= 2) 
+    {
+        for(startPos = 0; startPos < partitionSize; startPos += 2 * currentSize) 
+        {
+            midpointPos = Math.min(startPos + currentSize, partitionSize);
+            endPos = Math.min(startPos + 2 * currentSize, partitionSize);
+            
+            leftPos = startPos; 
+            rightPos = midpointPos;     
+            
+            var endPosFixed = endPos;
+          
+            while((leftPos < midpointPos) && (rightPos < endPosFixed))
+            {
+                if(partition[leftPos] <= partition[rightPos]) 
+                {
+                    leftPos++;
+                } 
+                  
+                else 
+                {
+                    temp = partition[rightPos];
+                    var startShiftPos = rightPos;
+                    
+                    for(var i = startShiftPos; i > leftPos; i--) 
+                    {
+                        partition[i] = partition[i - 1];
+                    }
+                    
+                    partition[leftPos] = temp;
+                    leftPos++;
+                    midpointPos++; 
+                    rightPos++;
+                }
+            }
+        }
+    }
+    
+    return partition;
+}
+
+
+////
+
+
+
+function reduceMerge(leftPartition, rightPartition) 
+{
+    var mergedResult = [];
+    var leftPos = 0;
+    var rightPos = 0;
+    var leftLength = leftPartition.length;
+    var rightLength = rightPartition.length;
+  
+  //
+    
+    while((leftPos < leftLength) && (rightPos < rightLength)) 
+    {
+        if(leftPartition[leftPos] <= rightPartition[rightPos])
+        {
+            mergedResult.push(leftPartition[leftPos]);
+            leftPos++;
+        }
+          
+        else
+        {
+            mergedResult.push(rightPartition[rightPos]);
+            rightPos++;
+        }
+    }
+    
+    while(leftPos < leftLength)
+    {
+        mergedResult.push(leftPartition[leftPos]);
+        leftPos++;
+    }
+    
+    while(rightPos < rightLength)
+    {
+        mergedResult.push(rightPartition[rightPos]);
+        rightPos++;
+    }
+    
+    return mergedResult;
+}
+
+
+
+////
