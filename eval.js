72e9254664f5a7d883eef31b0c220d8ea7bb8e3b
Kane Kriz <kanemkriz@gmail.com>
2025-05-11 11:23:37 -0600

    Update eval.js

--- eval.js
+++ eval.js
@@ -3,16 +3,25 @@
 //
 
 
-/* eslint no-eval: off, no-restricted-globals: off */
-
+/* eslint-disable no-restricted-globals */
 const isNode = typeof module !== 'undefined' && module.exports;
 
 if (isNode) {
-  process.once('message', code => {
-    eval(JSON.parse(code).data);
+  process.on('message', (code) => {
+    try {
+      const result = eval(code.data);
+      process.send(result);
+    } catch (error) {
+      process.send({ error: error.message });
+    }
   });
 } else {
-  self.onmessage = function onmessage(code) {
-    eval(code.data);
+  self.onmessage = function(e) {
+    try {
+      const result = eval(e.data);
+      self.postMessage(result);
+    } catch (error) {
+      self.postMessage({ error: error.message });
+    }
   };
 }
