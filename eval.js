65518252694655d87835761d1ddca1ccde47bdd4
Kane Kriz <kanemkriz@gmail.com>
2025-05-11 11:00:33 -0600

    Create eval.js

---
+++ eval.js
@@ -0,0 +1,18 @@
+// not my code. From the npm paralleljs website, cited
+
+//
+
+
+/* eslint no-eval: off, no-restricted-globals: off */
+
+const isNode = typeof module !== 'undefined' && module.exports;
+
+if (isNode) {
+  process.once('message', code => {
+    eval(JSON.parse(code).data);
+  });
+} else {
+  self.onmessage = function onmessage(code) {
+    eval(code.data);
+  };
+}
